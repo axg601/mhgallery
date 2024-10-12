616da8d89d00537009d62b62dafa911589501140
Ashton George <ashtongeorge17@gmail.com>
2024-10-11 23:17:10 -0400

    added new panoramics

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,30 +15,46 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-	<h1 class="bricolage-grotesque-large">About this site</h1>
-	<p>
-		This site is a showcase of student work from WRIT-102: Writing About Medicine at Saint Francis University.
-		The work was done to increase the livelihood of the elderly living at the Cambria Care Facility in Ebensburg, PA through botanical means. 
-	</p>
+<main>
+	<div class="text-column">
+		<h1 class="bricolage-grotesque-large">About this site</h1>
+		<p>
+			This site is a showcase of student work from WRIT-102: Writing About Medicine at Saint Francis University.
+			The work was done to increase the livelihood of the elderly living at the Cambria Care Facility in Ebensburg, PA through botanical means. 
+		</p>
+		<p>
+			This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+			following into your command line, with node package manager installed, and following the prompts:
+		</p>
+		<pre>npm create svelte@latest</pre>
+	</div>
 	<section class="explorer" style="">
-        <View360 class="is-16by9" projection={projection} gyro={true} autoplay={true}  />
+		<View360 
+			class="is-16by9" 
+			projection={projection} 
+			gyro={true} 
+			autoplay={false} 
+			initialPitch={20}
+			initialYaw={10}
+		/>
 	</section>
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line, with node package manager installed, and following the prompts:
-	</p>
-	<pre>npm create svelte@latest</pre>
-</div>
+</main>
+
 
 <style>
-	div {
-		padding: 1rem;
-	}
 	.explorer {
 		display: block;
-		max-width: 66%;
+		max-width: 33%;
 		margin: auto;
 		border: 1px solid black;
 	}
+	div {
+		padding: 1rem;
+	}
+	@media (max-width: 768px) {
+    .explorer {
+        width: 90%;
+		max-width: none;
+    }
+}
 </style>
--- src/routes/explorer/+page.svelte
+++ src/routes/explorer/+page.svelte
@@ -3,10 +3,22 @@
 
     import View360, { EquirectProjection } from "@egjs/svelte-view360";
     import "@egjs/svelte-view360/css/view360.min.css";
-    import panoImage from '$lib/images/pano2.jpg';
+    import corner from '$lib/images/corner.JPG';
+    import door from '$lib/images/door.JPG';
+    import parking from '$lib/images/parking.JPG';
+    import pavilion from '$lib/images/pavilion.JPG';
 
-    const projection = new EquirectProjection({
-        src: panoImage 
+    const cornerProjection = new EquirectProjection({
+        src: corner
+    });
+    const doorProjection = new EquirectProjection({
+        src: door
+    });
+    const parkingProjection = new EquirectProjection({
+        src: parking
+    });
+    const pavilionProjection = new EquirectProjection({
+        src: pavilion
     });
 </script>
 
@@ -17,18 +29,25 @@
 
 <main>
     <h2 class="bricolage-grotesque-medium">Check out the pavilion explorer below</h2>
-	<section class="explorer" style="">
-        <View360 class="is-16by9" projection={projection} gyro={true} autoplay={true}  />
-	</section>
+    <p>Click and drag to explore the 360° panorama image.</p>
+        <section class="explorer" style="">
+            <View360 class="is-16by9" projection={parkingProjection} gyro={true} autoplay={false}  />
+        </section>
+        <section class="explorer" style="">
+            <View360 class="is-16by9" projection={cornerProjection} gyro={true} autoplay={false}  />
+        </section>
+        <section class="explorer" style="">
+            <View360 class="is-16by9" projection={pavilionProjection} gyro={true} autoplay={true}  />
+        </section>
     <p>This explorer was built using <a href="https://naver.github.io/egjs-view360/">View360</a>, a 360° panorama image / video viewer.</p>
 </main>
 
 <style>
 .explorer {
-    /* display: flex; */
-    max-width: 66%;
+    width: 66%;
     margin: auto;
     border: 1px solid black;
+    margin-bottom: 2rem;
 }
 h2 {
     font-size: 2rem;
@@ -41,7 +60,8 @@ p {
 
 @media (max-width: 768px) {
     .explorer {
-        max-width: 90%;
+        width: 90%;
+        margin: none;
     }
 }
 
